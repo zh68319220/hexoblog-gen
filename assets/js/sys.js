14a83868dc05ced25fbf9ad5786a25960601eaa2
zhang.yufeng <420866750@qq.com>
2016-04-06 13:50:46 +0800

    yf: addblog

--- assets/js/sys.js
+++ assets/js/sys.js
@@ -9,6 +9,9 @@ $(function () {
             url: '/sys/addblog',
             type: 'POST',
             data: { html: $("#editor").html() },
+            beforeSend: function(xhr){
+
+            },
             success: function (data) {
 
             }
